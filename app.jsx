020cd1388d876f6bf85882320dc5699e6b427fa2
nhatpm3124 <nhatpm.sg@gmail.com>
2025-05-29 01:43:35 +0700

    new2

--- app.jsx
+++ app.jsx
@@ -5,7 +5,7 @@ export default function app() {
   return (
     <div>
       asasdasd
-      <h1>aaaaaádnkasjncjn kjsnj kj nsk nakjs nak jnaskjasn ksja naksj nákj nákj nákj nák jnaksj naksj na</h1>
+      <h1>aaaaaádnkj nák jnaksj naksj na</h1>
       <h2>lorem</h2>
     </div>
   )
